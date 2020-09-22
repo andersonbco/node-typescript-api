e842c0af6becbeede178665708f1744872975884
Anderson Correa <andersonbcorrea@hotmail.com>
2020-09-21 22:02:27 -0300

    setting up code style with prettier

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/51114250
 declare namespace NodeJS {
-    interface Global {
-        testRequest: import('supertest').SuperTest<import('supertest').Test>;
-    }
+  interface Global {
+    testRequest: import('supertest').SuperTest<import('supertest').Test>
+  }
 }
