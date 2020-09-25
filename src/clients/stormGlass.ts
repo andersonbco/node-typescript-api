8132e88dc0a31c29f1f150f362b130d7c4bd29c3
Anderson Correa <andersonbcorrea@hotmail.com>
2020-09-25 14:59:50 -0300

    Creating mocks with actual data from the StormGlass API

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,14 @@
+import { AxiosStatic } from 'axios'
+
 export class StormGlass {
+  readonly stormGlassParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection'
+  readonly stormGlassSource = 'noaa'
+
+  constructor(protected request: AxiosStatic) {}
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return Promise.resolve({})
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassParams}&source=${this.stormGlassSource}&end=1592113802&lat=${lat}&lng=${lng}`
+    )
   }
 }
