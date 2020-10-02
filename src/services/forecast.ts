76375caf1be3d8c3fcbbf45a6da78f01036eb9c2
Anderson Correa <andersonbcorrea@hotmail.com>
2020-10-01 21:11:06 -0300

    implementing the forecast service - WIP

---
+++ src/services/forecast.ts
@@ -0,0 +1,46 @@
+import { ForecastPoint, StormGlass } from '@src/clients/stormGlass'
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  name: string
+  position: BeachPosition
+  lat: number
+  lng: number
+  user: string
+}
+
+// Merge the Beach and ForecastPoint interfaces omitting the field 'user'
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<BeachForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = []
+    for (const beach of beaches) {
+      // query each beach forecast from the stormglass client
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
+      // merge the forecast and beach data into a single object
+      const enrichedBeachData = points.map((e) => ({
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1,
+        },
+        ...e,
+      }))
+      pointsWithCorrectSources.push(...enrichedBeachData)
+    }
+    return pointsWithCorrectSources
+  }
+}
