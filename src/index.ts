339ae7e6abaf5235d3911c734a475e1e5a9b94b8
Anderson Correa <anderson.correa@accenture.com>
2020-11-02 14:07:15 -0300

    implementing graceful shutdown and handling other error signals

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
 import { SetupServer } from './server'
 import config from 'config'
+import logger from './logger'
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promise: ${promise} and reason ${reason}`
+  )
+  // lets throw the error and let the uncaughtException handler below handle it
+  throw reason
+})
+
+process.on('uncaughtException', (error) => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`)
+  process.exit(ExitStatus.Failure)
+})
 ;(async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'))
-  await server.init()
-  server.start()
+  try {
+    const server = new SetupServer(config.get('App.port'))
+    await server.init()
+    server.start()
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+    exitSignals.map((sig) =>
+      process.on(sig, async () => {
+        try {
+          await server.close()
+          logger.info('App exited with success')
+          process.exit(ExitStatus.Success)
+        } catch (e) {
+          logger.error(`App exited with error ${e}`)
+          process.exit(ExitStatus.Failure)
+        }
+      })
+    )
+  } catch (e) {
+    logger.error(`App exited with error: ${e}`)
+    process.exit(ExitStatus.Failure)
+  }
 })()
